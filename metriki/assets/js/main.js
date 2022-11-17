febb57ef1a18da32aa4e8982b9524777afcc80a8
Boris200630 <116121246+Boris200630@users.noreply.github.com>
2022-11-17 09:10:54 +0300

    Add files via upload

---
+++ metriki/assets/js/main.js
@@ -0,0 +1,18 @@
+// Работа с классами
+
+// 1
+// var elem = document.querySelector("#elem");
+// elem.p.classList.add('www');
+// console.log(elem.classList);
+
+// 2
+// var elem = document.querySelector("#elem");
+// elem.classList.remove("www");
+// console.log(elem.classList);
+
+// 3
+
+
+
+
+
